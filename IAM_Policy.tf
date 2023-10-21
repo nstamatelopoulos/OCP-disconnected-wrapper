136d35930336cef38bc08736b9bc9c094b4aded9
nstamatelopoulos <nstamate@redhat.com>
2023-10-21 12:06:19 +0200

    Attaching IAM role was a dead-end, So i am trying with creating a user and access key.

--- IAM_Policy.tf
+++ IAM_Policy.tf
@@ -2,9 +2,9 @@
 # All policies required for the IAM role to deploy a cluster on AWS
 #=====================================================================================================================================
 
-resource "aws_iam_policy" "EC2_policy" {
-  name        = "EC2_policy"
-  description = "These are the required permissions to allow the installer manage the EC2 instances. Allows only in the created VPC"
+resource "aws_iam_user_policy" "Installer_policy_document" {
+  name        = "Installer_policy_document"
+  user        = aws_iam_user.Cluster_deployer.name
   policy      = <<EOF
 {
   "Version": "2012-10-17",
@@ -12,418 +12,63 @@ resource "aws_iam_policy" "EC2_policy" {
     {
       "Effect": "Allow",
       "Action": [
-        "ec2:AuthorizeSecurityGroupEgress",
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:CopyImage",
-        "ec2:CreateNetworkInterface",
-        "ec2:AttachNetworkInterface",
-        "ec2:CreateSecurityGroup",
-        "ec2:CreateTags",
-        "ec2:CreateVolume",
-        "ec2:DeleteSecurityGroup",
-        "ec2:DeleteSnapshot",
-        "ec2:DeleteTags",
-        "ec2:DeregisterImage",
-        "ec2:DescribeAccountAttributes",
-        "ec2:DescribeAddresses",
-        "ec2:DescribeAvailabilityZones",
-        "ec2:DescribeDhcpOptions",
-        "ec2:DescribeImages",
-        "ec2:DescribeInstanceAttribute",
-        "ec2:DescribeInstanceCreditSpecifications",
-        "ec2:DescribeInstances",
-        "ec2:DescribeInstanceTypes",
-        "ec2:DescribeInternetGateways",
-        "ec2:DescribeKeyPairs",
-        "ec2:DescribeNatGateways",
-        "ec2:DescribeNetworkAcls",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DescribePrefixLists",
-        "ec2:DescribeRegions",
-        "ec2:DescribeRouteTables",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeTags",
-        "ec2:DescribeVolumes",
-        "ec2:DescribeVpcAttribute",
-        "ec2:DescribeVpcClassicLink",
-        "ec2:DescribeVpcClassicLinkDnsSupport",
-        "ec2:DescribeVpcEndpoints",
-        "ec2:DescribeVpcs",
-        "ec2:GetEbsDefaultKmsKeyId",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ModifyNetworkInterfaceAttribute",
-        "ec2:RevokeSecurityGroupEgress",
-        "ec2:RevokeSecurityGroupIngress",
-        "ec2:RunInstances",
-        "ec2:TerminateInstances"
+      "ec2:*",
+      "elasticloadbalancing:*",
+      "iam:AddRoleToInstanceProfile",
+      "iam:CreateInstanceProfile",
+      "iam:CreateRole",
+      "iam:DeleteInstanceProfile",
+      "iam:DeleteRole",
+      "iam:DeleteRolePolicy",
+      "iam:GetInstanceProfile",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:GetUser",
+      "iam:ListInstanceProfilesForRole",
+      "iam:ListRoles",
+      "iam:ListUsers",
+      "iam:PassRole",
+      "iam:PutRolePolicy",
+      "iam:RemoveRoleFromInstanceProfile",
+      "iam:SimulatePrincipalPolicy",
+      "iam:TagRole",
+      "iam:CreateServiceLinkedRole",
+      "route53:ChangeResourceRecordSets",
+      "route53:ChangeTagsForResource",
+      "route53:CreateHostedZone",
+      "route53:DeleteHostedZone",
+      "route53:GetChange",
+      "route53:GetHostedZone",
+      "route53:ListHostedZones",
+      "route53:ListHostedZonesByName",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResource",
+      "route53:UpdateHostedZoneComment",
+      "s3:*",
+      "autoscaling:DescribeAutoScalingGroups",
+      "elasticloadbalancing:DeleteTargetGroup",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "iam:DeleteAccessKey",
+      "iam:DeleteUser",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfiles",
+      "iam:ListRolePolicies",
+      "iam:ListUserPolicies",
+      "tag:GetResources",
+      "iam:UntagRole",
+      "iam:DeleteAccessKey",
+      "iam:DeleteUser",
+      "iam:DeleteUserPolicy",
+      "iam:GetUserPolicy",
+      "iam:ListAccessKeys",
+      "iam:PutUserPolicy",
+      "iam:TagUser",
+      "servicequotas:ListAWSDefaultServiceQuotas"
       ],
+
       "Resource": "*"
     }
   ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Vpc_policy" {
-  name        = "Vpc_policy"
-  description = "These are the required permissions to allow the installer Create VPC and networking components. Allows everyone to be fixed"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:AllocateAddress",
-        "ec2:AssociateAddress",
-        "ec2:AssociateDhcpOptions",
-        "ec2:AssociateRouteTable",
-        "ec2:AttachInternetGateway",
-        "ec2:CreateDhcpOptions",
-        "ec2:CreateInternetGateway",
-        "ec2:CreateNatGateway",
-        "ec2:CreateRoute",
-        "ec2:CreateRouteTable",
-        "ec2:CreateSubnet",
-        "ec2:CreateVpc",
-        "ec2:CreateVpcEndpoint",
-        "ec2:ModifySubnetAttribute",
-        "ec2:ModifyVpcAttribute"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "LB_policy" {
-  name        = "LB_policy"
-  description = "These are the required permissions to allow the installer Manage LBs. Allows only in the created VPC"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "elasticloadbalancing:AddTags",
-                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-                "elasticloadbalancing:AttachLoadBalancerToSubnets",
-                "elasticloadbalancing:ConfigureHealthCheck",
-                "elasticloadbalancing:CreateLoadBalancer",
-                "elasticloadbalancing:CreateLoadBalancerListeners",
-                "elasticloadbalancing:DeleteLoadBalancer",
-                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-                "elasticloadbalancing:DescribeInstanceHealth",
-                "elasticloadbalancing:DescribeLoadBalancerAttributes",
-                "elasticloadbalancing:DescribeLoadBalancers",
-                "elasticloadbalancing:DescribeTags",
-                "elasticloadbalancing:ModifyLoadBalancerAttributes",
-                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                "elasticloadbalancing:SetLoadBalancerPoliciesOfListener"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "LBv2_policy" {
-  name        = "LBv2_policy"
-  description = "These are the required permissions to allow the installer Manage LBv2. Allows only in the created VPC"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "elasticloadbalancing:AddTags",
-                "elasticloadbalancing:CreateListener",
-                "elasticloadbalancing:CreateLoadBalancer",
-                "elasticloadbalancing:CreateTargetGroup",
-                "elasticloadbalancing:DeleteLoadBalancer",
-                "elasticloadbalancing:DeregisterTargets",
-                "elasticloadbalancing:DescribeListeners",
-                "elasticloadbalancing:DescribeLoadBalancerAttributes",
-                "elasticloadbalancing:DescribeLoadBalancers",
-                "elasticloadbalancing:DescribeTargetGroupAttributes",
-                "elasticloadbalancing:DescribeTargetHealth",
-                "elasticloadbalancing:ModifyLoadBalancerAttributes",
-                "elasticloadbalancing:ModifyTargetGroup",
-                "elasticloadbalancing:ModifyTargetGroupAttributes",
-                "elasticloadbalancing:RegisterTargets"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Iam_resources_policy" {
-  name        = "Iam_resources_policy"
-  description = "These are the required permissions to allow the installer Manage IAM."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:AddRoleToInstanceProfile",
-                "iam:CreateInstanceProfile",
-                "iam:CreateRole",
-                "iam:DeleteInstanceProfile",
-                "iam:DeleteRole",
-                "iam:DeleteRolePolicy",
-                "iam:GetInstanceProfile",
-                "iam:GetRole",
-                "iam:GetRolePolicy",
-                "iam:GetUser",
-                "iam:ListInstanceProfilesForRole",
-                "iam:ListRoles",
-                "iam:ListUsers",
-                "iam:PassRole",
-                "iam:PutRolePolicy",
-                "iam:RemoveRoleFromInstanceProfile",
-                "iam:SimulatePrincipalPolicy",
-                "iam:TagRole",
-                "iam:CreateServiceLinkedRole"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Route53_policy" {
-  name        = "Route53_policy"
-  description = "These are the required permissions to allow the installer Manage Route53."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "route53:ChangeResourceRecordSets",
-                "route53:ChangeTagsForResource",
-                "route53:CreateHostedZone",
-                "route53:DeleteHostedZone",
-                "route53:GetChange",
-                "route53:GetHostedZone",
-                "route53:ListHostedZones",
-                "route53:ListHostedZonesByName",
-                "route53:ListResourceRecordSets",
-                "route53:ListTagsForResource",
-                "route53:UpdateHostedZoneComment"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-
-resource "aws_iam_policy" "S3_policy" {
-  name        = "S3_policy"
-  description = "These are the required permissions to allow the installer Manage S3."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:CreateBucket",
-                "s3:DeleteBucket",
-                "s3:GetAccelerateConfiguration",
-                "s3:GetBucketAcl",
-                "s3:GetBucketCors",
-                "s3:GetBucketLocation",
-                "s3:GetBucketLogging",
-                "s3:GetBucketPolicy",
-                "s3:GetBucketObjectLockConfiguration",
-                "s3:GetBucketReplication",
-                "s3:GetBucketRequestPayment",
-                "s3:GetBucketTagging",
-                "s3:GetBucketVersioning",
-                "s3:GetBucketWebsite",
-                "s3:GetEncryptionConfiguration",
-                "s3:GetLifecycleConfiguration",
-                "s3:GetReplicationConfiguration",
-                "s3:ListBucket",
-                "s3:PutBucketAcl",
-                "s3:PutBucketTagging",
-                "s3:PutEncryptionConfiguration"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "S3_operators_policy" {
-  name        = "S3_operators_policy"
-  description = "These are the required permissions to allow the operators Manage S3."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:DeleteObject",
-                "s3:GetObject",
-                "s3:GetObjectAcl",
-                "s3:GetObjectTagging",
-                "s3:GetObjectVersion",
-                "s3:PutObject",
-                "s3:PutObjectAcl",
-                "s3:PutObjectTagging"
-            ],
-            "Resource": "arn:aws:s3:::your-bucket-name/*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Deleting_cluster_resources" {
-  name        = "Deleting_cluster_resources_policy"
-  description = "These are the required permissions to allow the user delete cluster resources."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "autoscaling:DescribeAutoScalingGroups",
-                "ec2:DeletePlacementGroup",
-                "ec2:DeleteNetworkInterface",
-                "ec2:DeleteVolume",
-                "elasticloadbalancing:DeleteTargetGroup",
-                "elasticloadbalancing:DescribeTargetGroups",
-                "iam:DeleteAccessKey",
-                "iam:DeleteUser",
-                "iam:ListAttachedRolePolicies",
-                "iam:ListInstanceProfiles",
-                "iam:ListRolePolicies",
-                "iam:ListUserPolicies",
-                "s3:DeleteObject",
-                "s3:ListBucketVersions",
-                "tag:GetResources"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Deleting_network_resources" {
-  name        = "Deleting_network_resources_policy"
-  description = "These are the required permissions to allow the user delete network resources."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:DeleteDhcpOptions",
-                "ec2:DeleteInternetGateway",
-                "ec2:DeleteNatGateway",
-                "ec2:DeleteRoute",
-                "ec2:DeleteRouteTable",
-                "ec2:DeleteSubnet",
-                "ec2:DeleteVpc",
-                "ec2:DeleteVpcEndpoints",
-                "ec2:DetachInternetGateway",
-                "ec2:DisassociateRouteTable",
-                "ec2:ReleaseAddress",
-                "ec2:ReplaceRouteTableAssociation"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-
-resource "aws_iam_policy" "Untag_Role" {
-  name        = "Untag_Role"
-  description = "These are the required permissions to allow the user Untag Role."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "iam:UntagRole",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Create_manifests_policy" {
-  name        = "Create_manifests_policy"
-  description = "These are the required permissions to allow the user Create manifests."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:DeleteAccessKey",
-                "iam:DeleteUser",
-                "iam:DeleteUserPolicy",
-                "iam:GetUserPolicy",
-                "iam:ListAccessKeys",
-                "iam:PutUserPolicy",
-                "iam:TagUser",
-                "s3:PutBucketPublicAccessBlock",
-                "s3:GetBucketPublicAccessBlock",
-                "s3:PutLifecycleConfiguration",
-                "s3:HeadBucket",
-                "s3:ListBucketMultipartUploads",
-                "s3:AbortMultipartUpload"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "Quotas_policy" {
-  name        = "Quotas_policy"
-  description = "These are the required permissions to allow the user to use quotas checks."
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:DescribeInstanceTypeOfferings",
-                "servicequotas:ListAWSDefaultServiceQuotas"
-            ],
-            "Resource": "*"
-        }
-    ]
 }
 EOF
 }
